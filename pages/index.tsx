7c11b4859b0dcf55ad9f58a9b707b0a65412f566
Lenka Shynkarova <lllee@post.cz>
2022-04-08 14:11:54 +0200

    cards colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,12 @@ const Home = () => {
     // flex-direction: column;
     display: grid;
     padding: 0 1.2rem;
+    row-gap: 1rem;
 
     ${mq.mediumUp.css`
   // flex-direction: row;
    grid-template-columns: 2fr 1fr 1fr;
+   grid-template-rows: 2fr;
     column-gap: 1rem;
     padding: 0 3rem;
  `}
@@ -24,9 +26,38 @@ const Home = () => {
   return (
     <>
       <TopSectionContainer>
-        <SquareBlock title="Jazyk" />
-        <SquareBlock title="Umění" />
-        <SquareBlock title="Hudba" />
+        <SquareBlock
+          title="Ukrajinské umění"
+          shortDescription="„Ukrajinské umění bylo vždy víc aktivistické, občanské,
+                promlouvalo o tom, co se kolem nás děje.“"
+          cardColor="#fec9e5"
+          cardTextColor="#3A6BA4"
+        />
+        <SquareBlock
+          title="Ukrajinský jazyk"
+          cardColor="#F4E2A3"
+          cardTextColor="#A74D4D"
+        />
+        <SquareBlock
+          title="Ukrajinská hudba"
+          cardColor="#CBA5FC"
+          cardTextColor="#595287"
+        />
+        <SquareBlock
+          title="Ukrajinská literatura"
+          cardColor="#79A4E7"
+          cardTextColor="#F4E2A3"
+        />
+        <SquareBlock
+          title="O projektu"
+          cardColor="#F3F3F3"
+          cardTextColor="#3A6BA4"
+        />
+        <SquareBlock
+          title="Články"
+          cardColor="#F3F3F3"
+          cardTextColor="#3A6BA4"
+        />
       </TopSectionContainer>
     </>
   );
--- src/components/SquareBlock/SquareBlock.tsx
+++ src/components/SquareBlock/SquareBlock.tsx
@@ -2,30 +2,60 @@ import { FC } from 'react';
 import styled, { css } from 'styled-components';
 import { mq } from '../../utils/styles';
 
-const SquareContainer = styled.div`
-  background-color: #fec9e5;
-  color: #3a6ba4;
+const SquareContainer = styled.div<{
+  cardColor: string;
+  cardTextColor: string;
+}>`
+  background-color: ${(props) => props.cardColor};
+  color: ${(props) => props.cardTextColor};
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const SquareTitle = styled.h2`
   font-size: 1.8rem;
+  min-height: 6rem;
   ${mq.mediumUp.css`
-  font-size: 4rem;
+  font-size: 3rem;
+  min-height: 6rem;
  `}
 `;
 
-const SquareContent = styled.div``;
+const SquareContent = styled.div`
+  padding: 1rem;
+  ${mq.mediumUp.css`
+  padding: 2rem;
+ `}
+`;
+
+const ShortDescription = styled.p`
+  font-size: 0.8rem;
+  font-weight: 600;
+  ${mq.mediumUp.css`
+  font-size: 1rem;
+ `}
+`;
 
 type SquareBlockProps = {
   title: string;
+  shortDescription?: string;
+  cardColor: string;
+  cardTextColor: string;
 };
 
-const SquareBlock: FC<SquareBlockProps> = ({ title }) => {
+const SquareBlock: FC<SquareBlockProps> = ({
+  title,
+  shortDescription,
+  cardColor,
+  cardTextColor,
+}) => {
   return (
-    <SquareContainer>
+    <SquareContainer cardColor={cardColor} cardTextColor={cardTextColor}>
       <SquareContent>
         <SquareTitle>{title}</SquareTitle>
+        <ShortDescription>{shortDescription}</ShortDescription>
       </SquareContent>
     </SquareContainer>
   );
